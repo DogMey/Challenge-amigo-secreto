6c6c092378b343566834836c12111bad02cc6b32
DogMey <kevinsantiagoram25@gmail.com>
2025-03-03 22:02:34 -0500

    Se completo los requerimientos del proyecto

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos =[];
+
+function sortearAmigo(){
+    let amigoSecreto=amigos[Math.floor(Math.random()*amigos.length)];
+    document.getElementById("resultado").innerHTML = amigoSecreto;
+}
+
+function agregarAmigo(){
+    getAmigo();
+    mostrarAmigos();
+    return amigos;
+}
+
+function getAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+    if(nombreAmigo == ""){
+        alert("Debe ingresar un nombre");
+    }else{
+        document.getElementById("amigo").value = "";
+        amigos.push(nombreAmigo);
+        return nombreAmigo;
+    }
+}
+
+function mostrarAmigos(){
+    document.getElementById("listaAmigos").innerHTML = "";
+    amigos.forEach (amigo => {
+        document.getElementById("listaAmigos").innerHTML += amigo + "<br>";
+    });
+}
